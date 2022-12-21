f83b8cb16e1c3e2bb45c4def6cfe28859a714367
DanyaSht <danyloshtog@gmail.com>
2022-12-21 15:23:35 +0200

    Adding edited file Movie.h

--- Movie.h
+++ Movie.h
@@ -6,77 +6,77 @@ using namespace std;
 class Movie
 {
 public:
-	int id;
-	string title;
-	string ranking;
-	int release_date;
-	int tickets;
-	int ticket_price;
-	string comment;
+  int id;
+  string title;
+  string ranking;
+  int release_date;
+  int tickets;
+  int ticket_price;
+  string comment;
 
-	int getId()
-	{
-		return id;
-	}
-	string getTitle()
-	{
-		return title;
-	}
-	string getRanking()
-	{
-		return ranking;
-	}
-	int getReleaseDate()
-	{
-		return release_date;
-	}
-	int getNumberOfTickets()
-	{
-		return tickets;
-	}
-	int getTicketPrice()
-	{
-		return ticket_price;
-	}
-	string getComment()
-	{
-		return comment;
-	};
+  int getId()
+  {
+    return id;
+  }
+  string getTitle()
+  {
+    return title;
+  }
+  string getRanking()
+  {
+    return ranking;
+  }
+  int getReleaseDate()
+  {
+    return release_date;
+  }
+  int getNumberOfTickets()
+  {
+    return tickets;
+  }
+  int getTicketPrice()
+  {
+    return ticket_price;
+  }
+  string getComment()
+  {
+    return comment;
+  };
 
-	Movie() {};
+  Movie() {};
 
-	enum filmType
-	{
-		Action, Comedy, Drama, Fantasy
-	};
+  enum filmType
+  {
+    Action, Comedy, Drama, Fantasy
+  };
 
-	void calculateProfit()
-	{
-		int a = tickets;
-		int b = ticket_price;
-		int c = a * b;
-		cout << "Profit for a day : " << c << endl;
-	};
+  void calculateProfit()
+  {
+    int a = tickets;
+    int b = ticket_price;
+    int c = a * b;
+    cout << "Profit for a day : " << c << endl;
+  };
 
-	Movie(int aId, string aTitle, string aRanking, int aRelease_date, int aTickets, int aTicket_price, string aComment)
-	{
-		id = aId;
-		title = aTitle;
-		ranking = aRanking;
-		release_date = aRelease_date;
-		tickets = aTickets;
-		ticket_price = aTicket_price;
-		comment = aComment;
-	}
+  Movie(int kMovieId, string kMovieTitle, string kMovieRanking, int kMovieReleaseDate, int kMovieTickets, int kTicketPrice, string kMovieComment)
+  {
+    id = kMovieId;
+    title = kMovieTitle;
+    ranking = kMovieRanking;
+    release_date = kMovieReleaseDate;
+    tickets = kMovieTickets;
+    ticket_price = kTicketPrice;
+    comment = kMovieComment;
+  }
 
-	void Print()
-	{
-		cout << "Id:\t\n" << id << endl;
-		cout << "Title:" << title << endl;
-		cout << "Ranking:" << ranking << endl;
-		cout << "Release date:" << release_date << endl;
-		cout << "Number of tickets:" << tickets << endl;
-		cout << "Ticket price:" << ticket_price << endl;
-		cout << "Comment:" << comment << endl;
-	}
+  void Print()
+  {
+    cout << "Id:\t\n" << id << endl;
+    cout << "Title:" << title << endl;
+    cout << "Ranking:" << ranking << endl;
+    cout << "Release date:" << release_date << endl;
+    cout << "Number of tickets:" << tickets << endl;
+    cout << "Ticket price:" << ticket_price << endl;
+    cout << "Comment:" << comment << endl;
+  }
 };
